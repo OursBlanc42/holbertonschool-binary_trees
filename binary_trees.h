97257decb9448ea1a9b212d65f017df430f6478f
OursBlanc <regniersimon@hotmail.com>
2024-12-09 16:50:17 +0100

    save local modification

--- binary_trees.h
+++ binary_trees.h
@@ -49,4 +49,9 @@ int binary_tree_is_root(const binary_tree_t *node);
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 
+
+
+
+size_t binary_tree_size(const binary_tree_t *tree);
+
 #endif
