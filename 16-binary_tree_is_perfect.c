9be14450417d9cefd3ce214de5a44f9d37ffe240
Gabriel-Nct <bescondgabriel@gmail.com>
2024-12-12 13:57:12 +0100

    fixed task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,50 +1,17 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 /**
-* height_calculation - count the height of a tree
-* Description : calculate the height by traversing each branch
-* keep a track of height and update it at each level
-* @node: is a pointer to a node of the tree
-* @L_height: is a counter to count each branch on left
-* @R_height: is a counter to count each branch on right
-* Return: NULL if tree is empty or size of tree
-*/
-int height_calculation(const binary_tree_t *node,
-int *L_height, int *R_height)
-{
-	/* If we reach NULL it's the end */
-	if (node == NULL)
-	{
-		return (0);
-	}
-
-	/* recursively catch each node */
-	*L_height = height_calculation(node->left, L_height, R_height);
-	*R_height = height_calculation(node->right, L_height, R_height);
-
-	/* find the maximum between two value */
-	if (*L_height >= *R_height)
-	{
-		return (*L_height + 1);
-	}
-	else
-	{
-		return (*R_height + 1);
-	}
-
-}
-
-/**
 * binary_tree_height - measures the height of a binary tree
+* Description: Call the subfunction "calculate_height" to retrieve height
+* and substract 1 (cause height start at 0 in exemple )
 * @tree: is a pointer to the root node of the tree to measure the height.
-* Return: Size of the tree of 0 if tree is NULL
+* Return: height of the node or 0 if tree is NULL or 1 leave
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	/* declare variable */
-	int L_height = 0;
-	int R_height = 0;
-	int height = 0;
+	/* declare and initialize variable */
+	size_t left_height = 0;
+	size_t right_height = 0;
 
 	/* Check special case */
 	if (tree == NULL)
@@ -52,11 +19,25 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	}
 
-	/* call recursive function & return result (-1 to convert len to index) */
-	height = (height_calculation(tree, &L_height, &R_height)) - 1;
-	return ((size_t)height);
-}
+	/* check special case when only one node height = 0;*/
+	if ((tree->left == NULL) && (tree->right == NULL))
+		return (0);
 
+	/* recursive call on each branch (left & right )*/
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	/* Find the max "len" between left and right subbranche */
+	if (left_height >= right_height)
+	{
+		return (left_height + 1);
+	}
+	else
+	{
+		return (right_height + 1);
+	}
+
+}
 /**
 * binary_tree_is_perfect - verify if a binary tree is perfect
 * @tree: pointer to the binary tree
